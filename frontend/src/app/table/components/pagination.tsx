b6acdceda7b355fc36cc747d38e94407f8fbe2e5
adorque <160372920+adorque@users.noreply.github.com>
2025-11-25 13:33:40 -0800

    Deleted repeated files

--- frontend/src/app/table/components/pagination.tsx
+++ frontend/src/app/table/components/pagination.tsx
@@ -7,7 +7,7 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "./ui/pagination"
 
 interface PaginationProps {
   currentPage: number;
